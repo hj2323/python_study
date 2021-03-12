bf8d70ae7e6916d3d50d144caeb3d75931921166
hyeji choi <hjchoi2323@gmail.com>
2021-03-12 16:57:24 +0900

    first commit

---
+++ variables.py
@@ -0,0 +1,43 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:37:30) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/chj/python_study/variables.py ==================
+>>> 
+=================== RESTART: C:/chj/python_study/variables.py ==================
+>>> 7.5+2.5 #덧셈
+10.0
+>>> 7.5-2.5 #뺄셈
+5.0
+>>> 7.5*2.5 #곱셈
+18.75
+>>> 7.5/2.5 #나눗셈
+3.0
+>>> 7.5//2.5 #몫
+3.0
+>>> 7.5%2.5 #나머지
+0.0
+>>> ""
+''
+>>> ''
+''
+>>> num=3
+>>> num]
+SyntaxError: unmatched ']'
+>>> num
+3
+>>> num=3.5
+>>> num
+3.5
+>>> type(num)
+<class 'float'>
+>>> a=3
+>>> isinstance(a, int)
+True
+>>> b=3.5
+>>> isinstance(b, float)
+True
+>>> name='최혜지'
+>>> type(name)
+<class 'str'>
+>>> var=''
+type(var)
